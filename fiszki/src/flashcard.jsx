1982960db1ac0b3e229ce40a4caa80af0d072f90
1Kr3ksu <bielawskowski@gmail.com>
2025-09-17 13:27:40 +0200

    licznik odtworzeń pojedynczej fiszki

--- fiszki/src/flashcard.jsx
+++ fiszki/src/flashcard.jsx
@@ -4,15 +4,21 @@ import './Flashcard.css'
 
 function Flashcard({question, answer}) {
     const [isFlipped , setIsFlipped] = useState(false);
+      const [flipCount, setFlipCount] = useState(0);
 
     function handleFlip(){
         setIsFlipped(!isFlipped)
+          setFlipCount(flipCount + 1)
     }
 return(
     <div className='flashcard'>
-        
-        {isFlipped ?  <p>{answer}✅</p> :  <p>{question}    </p> }
+         <p className="flip-count">
+        Otworzono fiszkę {flipCount} {flipCount === 1 ? 'raz' : 'razy'}
+      </p>
+    
+        {isFlipped ?  <p>{answer}</p> :  <p>{question}    </p> }
         <button onClick={handleFlip} className='przycisk'>Odwróć kartę</button>
+        
     </div>
 )
 }
