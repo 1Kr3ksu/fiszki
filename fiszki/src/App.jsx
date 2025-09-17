4a58c9435b57a13f436047416fa5262750be6565
1Kr3ksu <bielawskowski@gmail.com>
2025-09-17 12:16:36 +0200

    first commit

---
+++ fiszki/src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import Flashcard from './flashcard'
+
+function App() {
+  return(
+<Flashcard />
+  )
+}
+
+export default App
---
+++ fiszki/src/flashcard.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function Flashcard() {
+return(
+    <div className='flashcard'>
+        <p>Data wybuchu 2 wojny światowej</p>
+        <p>1 września 1939</p>
+        <button>Odwróć kartę</button>
+    </div>
+)
+}
+
+export default Flashcard
